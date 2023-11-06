095c6332620d5de69befa14c7cfcab20020f2140
Prakash <prakashku5566singh@gmail.com>
2023-11-06 20:17:44 +0530

    updated url for mongodb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import router from './routes/hrRoute.js';
-import mongoose from 'mongoose'
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/hrRoute.js";
+import mongoose from "mongoose";
 const app = express();
 
 // const multer = require("multer")
@@ -11,23 +11,28 @@ app.use(express.json());
 // app.use(multer().any());
 dotenv.config();
 
-mongoose.connect("mongodb+srv://Madhuri:Madhuri123@cluster0.r0zar1q.mongodb.net/Xperion", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
+mongoose
+  .connect(
+    "mongodb+srv://Madhuri9988:qkqCkGvAjbIv4Sv7@cluster0.stqyao7.mongodb.net/Xperian",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log("mongoose connection error", err));
 
 // app.use('/api/hr', router);
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-app.use( (req, res)=> {
-  var err = new Error('Not Found');
+app.use((req, res) => {
+  var err = new Error("Not Found");
   err.status = 404;
-  return res.status(404).send({status : "404 ", msg : "Path not found"})
-  });
+  return res.status(404).send({ status: "404 ", msg: "Path not found" });
+});
 
-app.listen(3001)
+app.listen(3001, () => {
+  console.log(`Server running on port 3001`);
+});
